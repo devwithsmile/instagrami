77ef4bb7b09bb425d13ad75daf4cc3f3c987e957
Shubhada <shubhada1310@gmail.com>
2023-10-19 15:53:15 +0530

    folders and instructions changed

--- src/main/java/com/spring_security_app/demo/Controllers/ChatGPT/CustomBotController.java
+++ src/main/java/com/spring_security_app/demo/Controllers/ChatGPT/CustomBotController.java
@@ -1,7 +1,7 @@
-package com.spring_security_app.demo.Controllers;
+package com.spring_security_app.demo.Controllers.ChatGPT;
 
-import com.spring_security_app.demo.DTO.ChatGPTRequest;
-import com.spring_security_app.demo.DTO.ChatGptResponse;
+import com.spring_security_app.demo.DTO.ChatGPT.ChatGPTRequest;
+import com.spring_security_app.demo.DTO.ChatGPT.ChatGptResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -27,11 +27,13 @@ public class CustomBotController {
     private RestTemplate template;
 
     @GetMapping()
-    public List<String> chat(@RequestParam("prompt") String prompt) {
+    public List<String> chat(@RequestParam("photoDescription") String photoDescription) {
         int responseCount = 3; // Set the number of responses you want here
         List<String> responses = new ArrayList<>();
 
         for (int i = 0; i < responseCount; i++) {
+            // Include the instruction along with the user's input
+            String prompt = "\"Imagine you're the ultimate Instagram caption maestro. I need you to craft a captivating and fancy caption, sprinkled with emojis, for a photo of around 10 to 12 words. Here is the description of photo  : " + photoDescription;
             ChatGPTRequest request = new ChatGPTRequest(model, prompt);
             ChatGptResponse chatGptResponse = template.postForObject(apiURL, request, ChatGptResponse.class);
             responses.add(chatGptResponse.getChoices().get(0).getMessage().getContent());
